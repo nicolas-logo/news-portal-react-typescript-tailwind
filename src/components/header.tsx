0e735513fec25e90bd93120edb223d7c10ac43ea
Nicolás Logosettis <logo.ncls@gmail.com>
2023-10-12 02:20:52 -0300

    prettier

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,13 @@
-
-import logo from "./../assets/images/logo.svg" 
-import Navbar from "../components/navbar" 
+import logo from "./../assets/images/logo.svg";
+import Navbar from "../components/navbar";
 
 const header = () => {
   return (
     <header className="flex place-content-between items-center mb-14">
-      <img src={logo} alt="Logo" className="" /> 
+      <img src={logo} alt="Logo" className="" />
       <Navbar />
     </header>
-  )
-}
+  );
+};
 
-export default header
+export default header;
